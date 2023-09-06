95e3221be5ae6fc0f5aa1b591da1b1af622102fa
Erastus Murungi <erastuswagwan@gmail.com>
2023-09-05 22:45:27 -0700

    started adding a token class

---
+++ include/Token.h
@@ -0,0 +1,14 @@
+//
+// Created by Erastus Murungi on 9/5/23.
+//
+
+#ifndef DECAF_TOKEN_H
+#define DECAF_TOKEN_H
+
+
+class Token {
+
+};
+
+
+#endif //DECAF_TOKEN_H
---
+++ src/basic/Token.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erastus Murungi on 9/5/23.
+//
+
+#include "Token.h"
